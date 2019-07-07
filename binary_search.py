8569664c492c0c8c8ce90522248c243f73ab8e38
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2019-07-07 23:11:00 +0300

    Process Time estimation has been added

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,11 @@
 import logging
+import random
+import time
 
 logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s]:[%(levelname)s]:[MESSAGE] %(message)s')
 
 
-def binary_search(array: list, target: int) -> (int, None):
+def binary_search(array: tuple, target: int) -> (int, None):
     left_side, right_side = 0, len(array) - 1
     n = 0
     while left_side <= right_side:
@@ -20,10 +22,21 @@ def binary_search(array: list, target: int) -> (int, None):
 
 
 if __name__ == "__main__":
-    lst = [num for num in range(0, 240000)]
-    number = 11
+    ar_start = time.process_time()
+    lst = tuple(num * random.randint(2, 5) for num in range(10000000))
+    ar_stop = time.process_time()
+    logging.info('Tuple has been created. Time: %s', ar_stop - ar_start)
+    number = lst[random.randint(0, len(lst) - 1)]
+    start = time.process_time()
     result, n = binary_search(array=lst, target=number)
+    stop = time.process_time()
+
     if result:
-        logging.info("Target '%s' on %s position in array. %s attempts has been made" % (number, result, n))
+        logging.info(
+            "Target '%s' on %s position in array. %s attempts has been made. Time: %s seconds.",
+            number, result, n, stop - start
+        )
     else:
-        logging.info("Target '%s' not found,  %s attempts has been made" % (number, n))
+        logging.info(
+            "Target '%s' not found. %s attempts has been made. Time: %s seconds.", number, n, stop - start
+        )
